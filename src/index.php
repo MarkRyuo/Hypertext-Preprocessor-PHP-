18ab13b580a3a1097a87dde10c31ead64635ac9b
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-01-14 09:43:09 +0800

    update herE

--- src/index.php
+++ src/index.php
@@ -4,13 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="main.css">
+  <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <title>Hypertext Propressor</title>
 </head>
 <body>
   
-  <section class="" id="">
+  <section class="" id="form">
 
-    <div class="container-lg">
+    <div class="container-lg my-3">
       <form action="index.php" method="post">
     
         <label for="fname">Name</label>
